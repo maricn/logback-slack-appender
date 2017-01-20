3c962bf56b2879cb26d30123f94df424e17ece9b
Nikola <nikola.maric@auto1.com>
2017-01-20 18:17:21 +0100

    Reformatting code.
    (cherry picked from commit 69dd6071b47dfe2caac075054660df94e2185b1c)

--- src/main/java/com/github/maricn/logback/SlackAppender.java
+++ src/main/java/com/github/maricn/logback/SlackAppender.java
@@ -1,18 +1,17 @@
 package com.github.maricn.logback;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.LayoutBase;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,14 +21,9 @@ public class SlackAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
     private final static String API_URL = "https://slack.com/api/chat.postMessage";
     private static Layout<ILoggingEvent> defaultLayout = new LayoutBase<ILoggingEvent>() {
         public String doLayout(ILoggingEvent event) {
-            StringBuffer sbuf = new StringBuffer(128);
-            sbuf.append("-- [");
-            sbuf.append(event.getLevel());
-            sbuf.append("]");
-            sbuf.append(event.getLoggerName());
-            sbuf.append(" - ");
-            sbuf.append(event.getFormattedMessage().replaceAll("\n", "\n\t"));
-            return sbuf.toString();
+            return "-- [" + event.getLevel() + "]" +
+                    event.getLoggerName() + " - " +
+                    event.getFormattedMessage().replaceAll("\n", "\n\t");
         }
     };
 
@@ -46,29 +40,30 @@ public class SlackAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
         try {
             final URL url = new URL(API_URL);
 
-            final StringWriter w = new StringWriter();
-            w.append("token=").append(token).append("&");
+            final StringWriter requestParams = new StringWriter();
+            requestParams.append("token=").append(token).append("&");
             String[] parts = layout.doLayout(evt).split("\n", 2);
-            w.append("text=").append(URLEncoder.encode(parts[0], "UTF-8")).append('&');
+            requestParams.append("text=").append(URLEncoder.encode(parts[0], "UTF-8")).append('&');
+
+            // Send the lines below the first line as an attachment.
             if (parts.length > 1) {
-                // Send the lines below the first line as an attachment.
                 Map<String, String> attachment = new HashMap<>();
                 attachment.put("text", parts[1]);
-                List<Map<String, String>> attachments = Arrays.asList(attachment);
+                List<Map<String, String>> attachments = Collections.singletonList(attachment);
                 String json = new ObjectMapper().writeValueAsString(attachments);
-                w.append("attachments=").append(URLEncoder.encode(json, "UTF-8")).append('&');
+                requestParams.append("attachments=").append(URLEncoder.encode(json, "UTF-8")).append('&');
             }
             if (channel != null) {
-                w.append("channel=").append(URLEncoder.encode(channel, "UTF-8")).append('&');
+                requestParams.append("channel=").append(URLEncoder.encode(channel, "UTF-8")).append('&');
             }
             if (username != null) {
-                w.append("username=").append(URLEncoder.encode(username, "UTF-8")).append('&');
+                requestParams.append("username=").append(URLEncoder.encode(username, "UTF-8")).append('&');
             }
             if (iconEmoji != null) {
-                w.append("icon_emoji=").append(URLEncoder.encode(iconEmoji, "UTF-8"));
+                requestParams.append("icon_emoji=").append(URLEncoder.encode(iconEmoji, "UTF-8"));
             }
 
-            final byte[] bytes = w.toString().getBytes("UTF-8");
+            final byte[] bytes = requestParams.toString().getBytes("UTF-8");
 
             final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(timeout);
